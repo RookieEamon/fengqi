66679c1f73150218602e6aa03eb99b78480438f3
yangzhiqiang <969994943@qq.com>
2020-10-19 18:01:50 +0800

    热点资讯页面(无分页器)

--- pinduoduo/src/api/index.js
+++ pinduoduo/src/api/index.js
@@ -1,3 +1,5 @@
 import ajax from './ajax'
 //发送请求获取热点资讯
 export const reqHotNews = ()=> ajax.get('/hotnews')
+//获取媒体报道列表
+export const reqMediaReports=(page,pageSize)=>ajax.get(`/mediareports/${page}/${pageSize}`)
