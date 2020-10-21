3bc227f3fa3bae490a6dc06d6280785b9650d4fe
LJM <15701204579@139.com>
2020-10-21 08:37:19 +0800

    更改

--- pinduoduo/src/api/index.js
+++ pinduoduo/src/api/index.js
@@ -9,7 +9,5 @@ export const reqHotpositionList = () => ajax.get('/hotposition')
 export const reqLatestPositionList = (job,page,pageSize) => ajax.post('/careers',{job,page,pageSize})
 //发送请求获取招聘详情列表
 export const reqJobdetail = (code) => ajax.post('/jobdetail',{code})
-//获取媒体报道列表
-export const reqMediaReports=(page,pageSize)=>ajax.get(`/mediareports/${page}/${pageSize}`)
 // 发送请求获取首页数据
 export const reqHome = () => ajax.get('/indexData')
